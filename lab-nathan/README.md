2ac873cf4b5d387c7f5c0943e979780a5522151e
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-07-19 15:11:43 -0700

    Adds get bytes.

---
+++ lab-nathan/README.md
@@ -0,0 +1,5 @@
+# get-bytes
+
+get-bytes is a node module which returns the specified number of bytes from the beginning of each input file.
+
+`done` is a function provided by the mocha testing framework, which is passed as a parameter of the `it` function's callback. It is invoked after asynchronous processing is complete and the result of the test has been computed.
--- lab-nathan/index.js
+++ lab-nathan/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const fp = require('./lib/fp.js');
+const getBytes = require('./lib/get-bytes.js');
 
-fp.map(process.argv.slice(2), a => console.log(a.toUpperCase()));
+let files = [`${__dirname}/assets/one.txt`, `${__dirname}/assets/two.txt`, `${__dirname}/assets/three.txt`];
+
+getBytes(files, 8, function(err, data) {
+  data.forEach(d => console.log(d));
+});
